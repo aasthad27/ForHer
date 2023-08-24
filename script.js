2801c56f2f9f1566f0c26b3f1c9058ed8eef888c
AASTHA <aasthad27@gmail.com>
2023-08-24 22:30:56 +0530

    "api key removed"

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
